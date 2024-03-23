54c15bef7d4523b76a650f589406beb5496dddd9
cyburns <72774499+cyburns@users.noreply.github.com>
2024-03-23 10:52:53 -0400

    sat

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,9 +7,9 @@ type Props = {
 const layout = ({ children }: Props) => {
   return (
     <>
-      <Sidebar />
-      <main className="pl-[256px] h-full">
-        <div className="bg-red-500 h-full">{children}</div>
+      <Sidebar className="hidden lg:flex" />
+      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+        <div className="max-w-[1056px] mx-auto pt-6 h-full">{children}</div>
       </main>
     </>
   );
